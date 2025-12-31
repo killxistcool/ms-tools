5a33be61ceba3bca688f400f6ce2a2b6ec162d0c
killxistcool <141137008+killxistcool@users.noreply.github.com>
2025-12-31 14:13:06 +0100

    Enhance script with user feedback messages
    Added echo statements for user feedback during cache clearing and reinstallation.

--- msrbx.sh
+++ msrbx.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 # clear rblx cache
+echo "Clearing Roblox Chache..."
 sudo rm -rf ~/Library/Caches/com.roblox.*
 sudo rm -rf ~/Library/Logs/Roblox
 # reinstall macsploit
+echo "Reinstalling MacSploit..."
 cd ~/ && curl -s "https://git.raptor.fun/main/install.sh" | bash </dev/tty
+echo "Successfully Roblox Cache and Reinstalled MacSploit."
